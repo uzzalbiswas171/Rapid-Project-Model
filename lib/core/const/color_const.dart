2156d23a326985c0c8c51a8c0cb877c07ce9d4ea
Your Name <you@example.com>
2025-08-30 16:12:09 +0600

    done added

--- lib/core/const/color_const.dart
+++ lib/core/const/color_const.dart
@@ -3,7 +3,7 @@
 import 'dart:ui';
 
 class ColorConst{
-  static const baseColor = Color(0xff468cf6);
+  static const baseColor = Color(0xff38BDF8);
   static const sliverAppbarColor = Color(0xffF6E2AD);
   static const foodCardBacColor = Color(0xffF9EBC8);
   static const defaultBackground = Color(0xFFFFF8E1);
